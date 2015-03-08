ad7a7f049b6115b175543f339488449a1950743f
KsasKia <saskia.kepp@gmx.net>
2015-03-08 14:55:00 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -38,17 +38,18 @@ cacheSolve <- function(x, ...) {
         
         # if the inverse has already been calculated
         if (!is.null(inv)){
-                # get it from the cache and skips the computation. 
+                # get it from the cache and skip the computation. 
                 message("getting cached data")
                 return(inv)
         }
         
-        # otherwise, calculates the inverse 
+        # otherwise, calculate the inverse 
         mat.data = x$get()
         inv = solve(mat.data, ...)
         
-        # sets the value of the inverse in the cache via the setinv function.
+        # set the value of the inverse in the cache via the setinv function.
         x$setinv(inv)
         
+        # return the inverse
         return(inv)
 }
